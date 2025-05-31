e0085bf816e6d526315634a1a68122dd82f03a9c
danielleit241 <danielleee241@gmail.com>
2025-05-31 21:01:42 +0700

    Add project files.

---
+++ DockerComposePotatoDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using DockerComposePotatoDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerComposePotatoDemo.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
